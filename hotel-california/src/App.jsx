984984402fc6e0ae68b83a121365d7b74461e12e
TomasT04 <tomas.trogliero04@gmail.com>
2025-10-07 23:53:40 -0300

    Prueba 4

--- hotel-california/src/App.jsx
+++ hotel-california/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Contacto from "./Pages/contacto";
+import Servicios from "./Pages/servicios";
+import Home from "./Pages/home";
 
+export default function App() {
   return (
+<<<<<<< Updated upstream
     <>
       <h1></h1>
       <h1>HOLIS</h1>
@@ -14,3 +19,22 @@ function App() {
 }
 
 export default App;
+=======
+    <Router>
+      {/* Menú de navegación */}
+      <nav style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/contacto">Contactosssss</Link>
+        <Link to="/servicios">Servicios</Link>
+      </nav>
+
+      {/* Definición de rutas */}
+      <Routes>
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
+  );
+}
+>>>>>>> Stashed changes
---
+++ hotel-california/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+
+/* Componente de Inicio */
+function Home() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Home</h1>
+      <h2>Cargador de Imagen </h2>
+      <ImageUploader />
+    </div>
+  );
+}
+export default Home;
